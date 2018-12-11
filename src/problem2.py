67b1d59d873fe5ce19f21605d99c5292132058bf
Lucas D'Alesio <dalesil@rose-hulman.edu>
2018-12-11 01:59:14 -0500

    problem 2 done

--- src/problem2.py
+++ src/problem2.py
@@ -3,8 +3,8 @@ PRACTICE Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Valerie Galluzzi, Mark Hays, Amanda Stouder, Aaron Wilkin,
-         their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues, and Lucas D'Alesio.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -103,7 +103,7 @@ def problem2a(circle, rectangle, window):
       :type window:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
@@ -111,6 +111,27 @@ def problem2a(circle, rectangle, window):
     #    DIFFICULTY:      6
     #    TIME ESTIMATE:   10 to 15 minutes.
     # -------------------------------------------------------------------------
+
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
+    x = rectangle.get_upper_right_corner().x
+    y = rectangle.get_upper_right_corner().y
+    x1 = rectangle.get_lower_left_corner().x
+    y1 = rectangle.get_lower_left_corner().y
+    line = rg.Line(rg.Point(x, y), rg.Point(x1, y1))
+    line.arrow = 'last'
+    line.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
+    circle.fill_color = rectangle.outline_color
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    line.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
+
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
@@ -174,7 +195,7 @@ def problem2b(rect, n, delta, win):
       :type win:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
@@ -183,6 +204,19 @@ def problem2b(rect, n, delta, win):
     #    TIME ESTIMATE:   15 to 25 minutes.
     # -------------------------------------------------------------------------
 
+    rect.attach_to(win)
+    x = rect.get_upper_right_corner().x
+    y = rect.get_upper_right_corner().y
+    x1 = rect.get_lower_left_corner().x
+    y1 = rect.get_lower_left_corner().y
+    for k in range(n):
+        x = x + delta
+        y = y - delta
+        x1 = x1 - delta
+        y1 = y1 + delta
+        rectangle = rg.Rectangle(rg.Point(x, y), rg.Point(x1, y1))
+        rectangle.attach_to(win)
+    win.render()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
